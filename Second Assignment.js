19f6fa16708780f363e707251b649559c6da990e
Tobi Awoniyi <Tobi.awoniyi@cerbalancetafrica.com>
2024-06-19 01:22:36 +0100

    first commit

---
+++ Second Assignment.js
@@ -0,0 +1,15 @@
+//  const age= 19
+//  if (age%2){
+//      console.log("age is odd")
+//  } else{
+//      console.log ("age is even")
+//      }
+
+ const age= 8
+if(number % 2 ==0){
+    console.log("number is even")
+}
+else{
+    console.log("number is odd")
+}
+        
---
+++ function.js
@@ -0,0 +1,36 @@
+// function total(c,d,e,f,g){
+//     console.log(c+d+e+f+g)
+// }
+// total(1,2,9,3,5,)
+
+// function subject (course){
+//     console.log(`am about to list out my courses called ${course}`)
+// }
+// subject('javascript')
+// subject('api testing')
+// subject('jira')
+
+// function multiply( w,x){
+//     console.log(w*x)
+// }
+//  multiply(10,4)
+//  multiply(5,4)
+//  multiply(1,4)
+
+//  function area(length,width) {
+//     console.log(`The area of the rectangle is: ${area}`);
+//   }
+//   area (10,5)
+//   area (11,5)
+//   area (12,5)
+// /  area (13,5)
+
+function area(length, width) {
+    console.log(length * width);
+    console.log(`The area of the rectangle is: ${length * width}`);
+  }
+  
+  area(13, 5);
+  area(12, 5);
+  area(11, 5);
+  
